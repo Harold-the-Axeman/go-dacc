25ac6ba66836e58fdd63b9592071d70665323e2a
bytejedi <me@bytejedi.com>
2018-09-29 14:47:21 +0800

    feat(cmd/puppeth): replace ethash to dpos

--- cmd/puppeth/wizard_node.go
+++ cmd/puppeth/wizard_node.go
@@ -22,7 +22,7 @@ import (
 	"time"
 
 	"github.com/daccproject/go-dacc/accounts/keystore"
-	"github.com/daccproject/go-dacc/common"
+	//"github.com/daccproject/go-dacc/common"
 	"github.com/daccproject/go-dacc/log"
 )
 
@@ -103,22 +103,23 @@ func (w *wizard) deployNode(boot bool) {
 	}
 	// If the node is a miner/signer, load up needed credentials
 	if !boot {
-		if w.conf.Genesis.Config.Ethash != nil {
-			// Ethash based miners only need an etherbase to mine against
-			fmt.Println()
-			if infos.etherbase == "" {
-				fmt.Printf("What address should the miner use?\n")
-				for {
-					if address := w.readAddress(); address != nil {
-						infos.etherbase = address.Hex()
-						break
-					}
-				}
-			} else {
-				fmt.Printf("What address should the miner use? (default = %s)\n", infos.etherbase)
-				infos.etherbase = w.readDefaultAddress(common.HexToAddress(infos.etherbase)).Hex()
-			}
-		} else if w.conf.Genesis.Config.Clique != nil {
+		//if w.conf.Genesis.Config.Ethash != nil {
+		//	// Ethash based miners only need an etherbase to mine against
+		//	fmt.Println()
+		//	if infos.etherbase == "" {
+		//		fmt.Printf("What address should the miner use?\n")
+		//		for {
+		//			if address := w.readAddress(); address != nil {
+		//				infos.etherbase = address.Hex()
+		//				break
+		//			}
+		//		}
+		//	} else {
+		//		fmt.Printf("What address should the miner use? (default = %s)\n", infos.etherbase)
+		//		infos.etherbase = w.readDefaultAddress(common.HexToAddress(infos.etherbase)).Hex()
+		//	}
+		//} else if w.conf.Genesis.Config.Clique != nil {
+		if w.conf.genesis.Config.Dpos != nil {
 			// If a previous signer was already set, offer to reuse it
 			if infos.keyJSON != "" {
 				if key, err := keystore.DecryptKey([]byte(infos.keyJSON), infos.keyPass); err != nil {
