ab229b70efe77d1bc3f1a8d66b2612555c8096c1
bytejedi <me@bytejedi.com>
2018-11-13 19:06:36 +0800

    feat(cmd/utils,node): change default http port to 8787, default websocket port to 8788, default p2p port to 30789

--- cmd/utils/flags.go
+++ cmd/utils/flags.go
@@ -503,7 +503,7 @@ var (
 	ListenPortFlag = cli.IntFlag{
 		Name:  "port",
 		Usage: "Network listening port",
-		Value: 30303,
+		Value: 30789,
 	}
 	BootnodesFlag = cli.StringFlag{
 		Name:  "bootnodes",
--- node/defaults.go
+++ node/defaults.go
@@ -29,9 +29,9 @@ import (
 
 const (
 	DefaultHTTPHost = "localhost" // Default host interface for the HTTP RPC server
-	DefaultHTTPPort = 8545        // Default TCP port for the HTTP RPC server
+	DefaultHTTPPort = 8787        // Default TCP port for the HTTP RPC server
 	DefaultWSHost   = "localhost" // Default host interface for the websocket RPC server
-	DefaultWSPort   = 8546        // Default TCP port for the websocket RPC server
+	DefaultWSPort   = 8788        // Default TCP port for the websocket RPC server
 )
 
 // DefaultConfig contains reasonable default settings.
