3704b019542a901f7e87277aa501d44d8eb4e2bd
bytejedi <me@bytejedi.com>
2018-11-22 14:56:49 +0800

    feat(miner): change default MinerGasFloor,MinerGasCeil to 12000000

--- eth/config.go
+++ eth/config.go
@@ -50,8 +50,8 @@ var DefaultConfig = Config{
 	DatabaseCache: 768,
 	TrieCache:     256,
 	TrieTimeout:   60 * time.Minute,
-	MinerGasFloor: 8000000,
-	MinerGasCeil:  8000000,
+	MinerGasFloor: 12000000,
+	MinerGasCeil:  12000000,
 	MinerGasPrice: big.NewInt(params.GWei),
 	MinerRecommit: 3 * time.Second,
 
