8088fc3d001655eca2c0a2b4c5a80c90bbe91bc2
bytejedi <me@bytejedi.com>
2018-12-20 16:18:53 +0800

    fix(internal): remove submitted tx log in ethapi

--- internal/ethapi/api.go
+++ internal/ethapi/api.go
@@ -1224,9 +1224,10 @@ func submitTransaction(ctx context.Context, b Backend, tx *types.Transaction) (c
 		}
 		addr := crypto.CreateAddress(from, tx.Nonce())
 		log.Info("Submitted contract creation", "fullhash", tx.Hash().Hex(), "contract", addr.Hex())
-	} else {
-		log.Info("Submitted transaction", "fullhash", tx.Hash().Hex(), "recipient", tx.To())
 	}
+	//} else {
+	//	log.Info("Submitted transaction", "fullhash", tx.Hash().Hex(), "recipient", tx.To())
+	//}
 	return tx.Hash(), nil
 }
 
