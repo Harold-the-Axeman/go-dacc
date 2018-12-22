5e8e05c2912e7978e4419ff46191b645f6e6e9ea
xl <xl@github.com>
2018-12-22 17:00:39 +0800

    feat(gas):update default gas

--- eth/config.go
+++ eth/config.go
@@ -50,8 +50,8 @@ var DefaultConfig = Config{
 	DatabaseCache: 768,
 	TrieCache:     256,
 	TrieTimeout:   60 * time.Minute,
-	MinerGasFloor: 110000000,
-	MinerGasCeil:  110000000,
+	MinerGasFloor: 220000000,
+	MinerGasCeil:  220000000,
 	MinerGasPrice: big.NewInt(params.GWei),
 	MinerRecommit: 3 * time.Second,
 
