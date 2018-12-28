c43c940810f26538570c648287ae5d509a3556fa
xl <xl@github.com>
2018-12-28 16:03:40 +0800

    fix(dpos):max validator 9

--- consensus/dpos/dpos.go
+++ consensus/dpos/dpos.go
@@ -33,7 +33,7 @@ const (
 
 	blockInterval    = int64(5)
 	epochInterval    = int64(86400)
-	maxValidatorSize = 5
+	maxValidatorSize = 9
 	safeSize         = maxValidatorSize*2/3 + 1
 	consensusSize    = maxValidatorSize*2/3 + 1
 )
